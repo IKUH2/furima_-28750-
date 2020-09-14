3b0ee2ace846c78da4d86f954cc33b6ed39df13e
IKUH2 <yikuh2@gmail.com>
2020-09-14 21:19:09 +0900

    binding.pry消し忘れ

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,7 +9,6 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_address_params)
-    binding.pry
     if @purchase_address.valid?
       pay_item
       @purchase_address.save
